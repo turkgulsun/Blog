674e146ba456bbee74910752546bfefa3086e0dd
Tuna <turkgulsun@gmail.com>
2019-09-06 17:39:33 +0300

    Home

--- Blog.WebUI/Areas/Admin/Controllers/HomeController.cs
+++ Blog.WebUI/Areas/Admin/Controllers/HomeController.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Blog.WebUI.Areas.Admin.Controllers
 {
-    [Route("Home")]
+    //[Route("Home")]
     public class HomeController : Controller
     {
         private IClassService _classesService;
